060f192356742a8aac3b4294f53b831460a896dc
manach <chris.scott22@gmail.com>
2021-10-27 21:38:47 -0400

    terraform for lambda, api, and DDB finished

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,7 +6,7 @@ terraform {
     }
   }
 
-  required_version = ">= 0.14.9"
+  required_version = ">= 1.0.9"
 }
 
 provider "aws" {
@@ -142,6 +142,139 @@ resource "aws_api_gateway_method" "post_method" {
 
 }
 
+//Then the OPTIONS method
+resource "aws_api_gateway_method" "options_method" {
+  rest_api_id   = aws_api_gateway_rest_api.cc_api.id
+  resource_id   = aws_api_gateway_resource.api_resource.id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+
+}
+
+resource "aws_api_gateway_integration" "options_integration" {
+  rest_api_id = aws_api_gateway_rest_api.cc_api.id
+  resource_id = aws_api_gateway_resource.api_resource.id
+  http_method = "OPTIONS"
+
+// OPTIONS doesn't call anything so it integrates to "MOCK"
+  type        = "MOCK"
+  depends_on  = [aws_api_gateway_method.options_method]
+
+  request_templates = {
+    "application/json" = jsonencode(
+      {
+        statusCode = 200
+      }
+    )
+  }
+}
+
+/*
+*****************************************
+CORS begins here. See https://mrponath.medium.com/terraform-and-aws-api-gateway-a137ee48a8ac
+*****************************************
+*/
+
+// Defining the method response for OPTIONS. This has to be set before the integration response. 
+resource "aws_api_gateway_method_response" "options_method_response" {
+  rest_api_id = aws_api_gateway_rest_api.cc_api.id
+  resource_id = aws_api_gateway_resource.api_resource.id
+  http_method = "OPTIONS"
+  status_code = "200"
+
+  response_models = {
+    "application/json" = "Empty"
+  }
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true,
+    "method.response.header.Access-Control-Allow-Methods" = true,
+    "method.response.header.Access-Control-Allow-Origin"  = true
+  }
+  depends_on = [aws_api_gateway_method.options_method]
+}
+
+// OPTIONS integration response. 
+resource "aws_api_gateway_integration_response" "options_integration_response" {
+  rest_api_id = aws_api_gateway_rest_api.cc_api.id
+  resource_id = aws_api_gateway_resource.api_resource.id
+  http_method = "OPTIONS"
+  status_code = "200"
+
+  response_parameters = {
+  "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+
+        "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST,PUT'",
+
+        "method.response.header.Access-Control-Allow-Origin" = "'https://manach.dev'"
+  }
+
+  // See https://stackoverflow.com/a/60015289/15301692
+  response_templates = {
+  "application/json" = <<EOF
+$input.json("$")
+#set($domains = ["https://www.ccsportfolio.com", "https://ccsportfolio.com", "https://www.manach.dev"])
+#set($origin = $input.params("origin"))
+#if($domains.contains($origin))
+#set($context.responseOverride.header.Access-Control-Allow-Origin="$origin")
+#end
+EOF
+}
+  depends_on = [aws_api_gateway_method_response.options_method_response,
+                aws_api_gateway_integration.options_integration]
+}
+
+// POST method response
+resource "aws_api_gateway_method_response" "post_method_response" {
+  rest_api_id = aws_api_gateway_rest_api.cc_api.id
+  resource_id = aws_api_gateway_resource.api_resource.id
+  http_method = "POST"
+  status_code = "200"
+
+  response_models = {
+    "application/json" = "Empty"
+  }
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = true
+  }
+  depends_on = [aws_api_gateway_method.post_method]
+}
+
+// Now we connect POST to the lambda
+resource "aws_api_gateway_integration" "post_integration" {
+  rest_api_id = aws_api_gateway_rest_api.cc_api.id
+  resource_id = aws_api_gateway_resource.api_resource.id
+  http_method = aws_api_gateway_method.post_method.http_method
+
+  integration_http_method = "POST"
+  type                    = "AWS" 
+  uri                     = aws_lambda_function.cloud_challenge_lambda.invoke_arn
+
+}
+
+// POST integration response
+resource "aws_api_gateway_integration_response" "post_integration_response" {
+  rest_api_id = aws_api_gateway_rest_api.cc_api.id
+  resource_id = aws_api_gateway_resource.api_resource.id
+  http_method = aws_api_gateway_method.post_method.http_method
+  status_code = "200"
+
+  response_templates = {
+  "application/json" = <<EOF
+$input.json("$")
+#set($domains = ["https://www.ccsportfolio.com", "https://ccsportfolio.com", "https://www.manach.dev"])
+#set($origin = $input.params("origin"))
+#if($domains.contains($origin))
+#set($context.responseOverride.header.Access-Control-Allow-Origin="$origin")
+#end
+EOF
+}
+  depends_on = [aws_api_gateway_method_response.post_method_response,
+                aws_api_gateway_integration.post_integration]
+
+}
+
+
 // Then we'll give the API permission to invoke the lambda
 resource "aws_lambda_permission" "allow_apigw" {
   statement_id  = "AllowExecutionFromAPIGateway"
@@ -152,17 +285,6 @@ resource "aws_lambda_permission" "allow_apigw" {
 
 }
 
-// Then we'll connect it to the lambda
-resource "aws_api_gateway_integration" "lambda_int" {
-  rest_api_id = aws_api_gateway_rest_api.cc_api.id
-  resource_id = aws_api_gateway_resource.api_resource.id
-  http_method = aws_api_gateway_method.post_method.http_method
-
-  integration_http_method = "POST"
-  type                    = "AWS" 
-  uri                     = aws_lambda_function.cloud_challenge_lambda.invoke_arn
-
-}
 
 // Next is the stage
 resource "aws_api_gateway_stage" "prod_stage" {
@@ -195,17 +317,17 @@ resource "aws_api_gateway_method_settings" "post_settings" {
 
 // Now we deploy the api, with a redeploy rule in case things change in the future
 resource "aws_api_gateway_deployment" "apideploy" {
-  depends_on  = [aws_api_gateway_integration.lambda_int]
+  depends_on  = [aws_api_gateway_integration.post_integration]
 
   rest_api_id = aws_api_gateway_rest_api.cc_api.id
-  stage_name  = "prod"
+  stage_name  = ""
 
   //redeploy if anything changes
   triggers = {
   redeployment = sha1(jsonencode([
       aws_api_gateway_resource.api_resource.id,
       aws_api_gateway_method.post_method.id,
-      aws_api_gateway_integration.lambda_int.id,
+      aws_api_gateway_integration.post_integration.id,
     ]))
   }
 }
