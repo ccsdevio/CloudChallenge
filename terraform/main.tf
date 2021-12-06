4d8c03a58a467f8059278148c618569fd78767a8
manach <chris.scott22@gmail.com>
2021-12-06 12:54:13 -0500

    Removes terraform fmt

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,29 +23,16 @@ provider "aws" {
 
 // Set up the DynamoDB table. 
 resource "aws_dynamodb_table" "ccddbtable" {
-  name            = "CloudChallengeTable"
-  hash_key        = "id"
+  name     = "CloudChallengeTable"
+  hash_key = "id"
   attribute {
     name = "id"
     type = "S"
   }
-  billing_mode = "PROVISIONED"
-  read_capacity = 1
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 1
   write_capacity = 1
 }
-
-// Initialize the only item we'll need. NOTE: this ended up resetting currentCount to 0 each time the terraform script ran. So we're just taking it out for now, and the problem of automating this is left as a shower-thought exercise. One solution: use put_item through a terraform-invoked shell script, a la https://jacob-hudson.github.io/terraform/aws/dynamodb/2020/04/27/terraform-bulk-upload.html
-// resource "aws_dynamodb_table_item" "visitor_count" {
-//   table_name  = aws_dynamodb_table.ccddbtable.name
-//   hash_key    = aws_dynamodb_table.ccddbtable.hash_key
-
-//   item = <<ITEM
-// {
-//   "id": {"S": "visitorCount"},
-//   "currentCount": {"N": "0"}
-// }
-// ITEM
-// }
 
 // IAM policy for lambda
 resource "aws_iam_role_policy" "cc_lambda_policy" {
@@ -66,11 +53,11 @@ resource "aws_iam_role" "cc_role" {
 // Create the lambda
 resource "aws_lambda_function" "cloud_challenge_lambda" {
 
-  function_name = "CloudChallengeLambda"
-  filename      = "lambda_payload.zip"
-  role          = aws_iam_role.cc_role.arn
-  handler       = "lambda_function.lambda_handler" 
-  runtime       = "python3.9"
+  function_name    = "CloudChallengeLambda"
+  filename         = "lambda_payload.zip"
+  role             = aws_iam_role.cc_role.arn
+  handler          = "lambda_function.lambda_handler"
+  runtime          = "python3.9"
   source_code_hash = filebase64sha256("lambda_payload.zip")
 
 }
@@ -163,9 +150,9 @@ resource "aws_api_gateway_integration" "options_integration" {
   resource_id = aws_api_gateway_resource.api_resource.id
   http_method = "OPTIONS"
 
-// OPTIONS doesn't call anything so it integrates to "MOCK"
-  type        = "MOCK"
-  depends_on  = [aws_api_gateway_method.options_method]
+  // OPTIONS doesn't call anything so it integrates to "MOCK"
+  type       = "MOCK"
+  depends_on = [aws_api_gateway_method.options_method]
 
   request_templates = {
     "application/json" = jsonencode(
@@ -209,16 +196,16 @@ resource "aws_api_gateway_integration_response" "options_integration_response" {
   status_code = "200"
 
   response_parameters = {
-  "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
 
-        "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST,PUT'",
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST,PUT'",
 
-        "method.response.header.Access-Control-Allow-Origin" = "'https://manach.dev'"
+    "method.response.header.Access-Control-Allow-Origin" = "'https://manach.dev'"
   }
 
-// See https://stackoverflow.com/a/60015289/15301692
+  // See https://stackoverflow.com/a/60015289/15301692
   response_templates = {
-  "application/json" = <<EOF
+    "application/json" = <<EOF
 $input.json("$")
 #set($domains = ["https://www.ccsportfolio.com", "https://ccsportfolio.com", "https://www.manach.dev"])
 #set($origin = $input.params("origin"))
@@ -226,9 +213,9 @@ $input.json("$")
 #set($context.responseOverride.header.Access-Control-Allow-Origin="$origin")
 #end
 EOF
-}
+  }
   depends_on = [aws_api_gateway_method_response.options_method_response,
-                aws_api_gateway_integration.options_integration]
+  aws_api_gateway_integration.options_integration]
 }
 
 // POST method response
@@ -254,7 +241,7 @@ resource "aws_api_gateway_integration" "post_integration" {
   http_method = aws_api_gateway_method.post_method.http_method
 
   integration_http_method = "POST"
-  type                    = "AWS" 
+  type                    = "AWS"
   uri                     = aws_lambda_function.cloud_challenge_lambda.invoke_arn
 
 }
@@ -267,7 +254,7 @@ resource "aws_api_gateway_integration_response" "post_integration_response" {
   status_code = "200"
 
   response_templates = {
-  "application/json" = <<EOF
+    "application/json" = <<EOF
 $input.json("$")
 #set($domains = ["https://www.ccsportfolio.com", "https://ccsportfolio.com", "https://www.manach.dev"])
 #set($origin = $input.params("origin"))
@@ -275,9 +262,9 @@ $input.json("$")
 #set($context.responseOverride.header.Access-Control-Allow-Origin="$origin")
 #end
 EOF
-}
+  }
   depends_on = [aws_api_gateway_method_response.post_method_response,
-                aws_api_gateway_integration.post_integration]
+  aws_api_gateway_integration.post_integration]
 
 }
 
@@ -295,7 +282,7 @@ resource "aws_lambda_permission" "allow_apigw" {
 
 // Next is the stage
 resource "aws_api_gateway_stage" "prod_stage" {
-  depends_on    = [aws_cloudwatch_log_group.prod_logs]
+  depends_on = [aws_cloudwatch_log_group.prod_logs]
 
   stage_name    = var.stage_name
   deployment_id = aws_api_gateway_deployment.apideploy.id
@@ -304,7 +291,7 @@ resource "aws_api_gateway_stage" "prod_stage" {
 
 // Now we set the log group
 resource "aws_cloudwatch_log_group" "prod_logs" {
-  name                  = "API-Gateway-Execution-Logs_${aws_api_gateway_rest_api.cc_api.id}/${var.stage_name}"
+  name = "API-Gateway-Execution-Logs_${aws_api_gateway_rest_api.cc_api.id}/${var.stage_name}"
 }
 
 // Now set the general method settings for logging. 
@@ -314,24 +301,24 @@ resource "aws_api_gateway_method_settings" "post_settings" {
   method_path = "*/*"
 
   settings {
-    data_trace_enabled      = false
-    metrics_enabled         = false
-    logging_level           = "INFO"
-    throttling_burst_limit  = 50
-    throttling_rate_limit   = 100
+    data_trace_enabled     = false
+    metrics_enabled        = false
+    logging_level          = "INFO"
+    throttling_burst_limit = 50
+    throttling_rate_limit  = 100
   }
 }
 
 // Now we deploy the api, with a redeploy rule in case things change in the future
 resource "aws_api_gateway_deployment" "apideploy" {
-  depends_on  = [aws_api_gateway_integration.post_integration]
+  depends_on = [aws_api_gateway_integration.post_integration]
 
   rest_api_id = aws_api_gateway_rest_api.cc_api.id
   stage_name  = ""
 
   //redeploy if anything changes
   triggers = {
-  redeployment = sha1(jsonencode([
+    redeployment = sha1(jsonencode([
       aws_api_gateway_resource.api_resource.id,
       aws_api_gateway_method.post_method.id,
       aws_api_gateway_integration.post_integration.id,
